d82e827dcace03709dc5e20cdff712045f381f9b
MAKSI <MAKSI@DESKTOP-ALQKQEP>
2024-04-19 19:23:46 +0300

    Добавьте файлы проекта.

---
+++ WinFormsApp2/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace WinFormsApp2
+{
+    partial class frmMass
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            txtn = new TextBox();
+            txtm = new TextBox();
+            txtRez = new TextBox();
+            dgvMass = new DataGridView();
+            cmdStart = new Button();
+            cmdExit = new Button();
+            cmdClear = new Button();
+            ((System.ComponentModel.ISupportInitialize)dgvMass).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(161, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Количество строк n=";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 105);
+            label2.Name = "label2";
+            label2.Size = new Size(166, 21);
+            label2.TabIndex = 1;
+            label2.Text = "Количество строк m=";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(12, 172);
+            label3.Name = "label3";
+            label3.Size = new Size(83, 21);
+            label3.TabIndex = 2;
+            label3.Text = "Результат:";
+            // 
+            // txtn
+            // 
+            txtn.Location = new Point(12, 53);
+            txtn.Name = "txtn";
+            txtn.Size = new Size(161, 29);
+            txtn.TabIndex = 3;
+            // 
+            // txtm
+            // 
+            txtm.Location = new Point(12, 129);
+            txtm.Name = "txtm";
+            txtm.Size = new Size(161, 29);
+            txtm.TabIndex = 4;
+            // 
+            // txtRez
+            // 
+            txtRez.Location = new Point(12, 209);
+            txtRez.Multiline = true;
+            txtRez.Name = "txtRez";
+            txtRez.Size = new Size(372, 290);
+            txtRez.TabIndex = 5;
+            // 
+            // dgvMass
+            // 
+            dgvMass.AllowUserToOrderColumns = true;
+            dgvMass.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvMass.Location = new Point(401, 20);
+            dgvMass.Name = "dgvMass";
+            dgvMass.Size = new Size(606, 479);
+            dgvMass.TabIndex = 6;
+            // 
+            // cmdStart
+            // 
+            cmdStart.Location = new Point(12, 519);
+            cmdStart.Name = "cmdStart";
+            cmdStart.Size = new Size(197, 35);
+            cmdStart.TabIndex = 7;
+            cmdStart.Text = "Вычислить";
+            cmdStart.UseVisualStyleBackColor = true;
+            cmdStart.Click += cmdStart_Click;
+            // 
+            // cmdExit
+            // 
+            cmdExit.Location = new Point(713, 519);
+            cmdExit.Name = "cmdExit";
+            cmdExit.Size = new Size(294, 35);
+            cmdExit.TabIndex = 8;
+            cmdExit.Text = "Завершить работу";
+            cmdExit.UseVisualStyleBackColor = true;
+            cmdExit.Click += cmdExit_Click;
+            // 
+            // cmdClear
+            // 
+            cmdClear.Location = new Point(282, 519);
+            cmdClear.Name = "cmdClear";
+            cmdClear.Size = new Size(326, 35);
+            cmdClear.TabIndex = 9;
+            cmdClear.Text = "Очистка полей";
+            cmdClear.UseVisualStyleBackColor = true;
+            cmdClear.Click += cmdClear_Click;
+            // 
+            // frmMass
+            // 
+            AutoScaleDimensions = new SizeF(9F, 21F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1029, 589);
+            Controls.Add(cmdClear);
+            Controls.Add(cmdExit);
+            Controls.Add(cmdStart);
+            Controls.Add(dgvMass);
+            Controls.Add(txtRez);
+            Controls.Add(txtm);
+            Controls.Add(txtn);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 12F);
+            Margin = new Padding(4);
+            Name = "frmMass";
+            Text = "Двумерные массивы";
+            Load += frmMass_Load;
+            ((System.ComponentModel.ISupportInitialize)dgvMass).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtn;
+        private TextBox txtm;
+        private TextBox txtRez;
+        private DataGridView dgvMass;
+        private Button cmdStart;
+        private Button cmdExit;
+        private Button cmdClear;
+    }
+}
---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,72 @@
+namespace WinFormsApp2
+{
+    public partial class frmMass : Form
+    {
+        public frmMass()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMass_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void cmdClear_Click(object sender, EventArgs e)
+        {
+            txtm.Text = "";
+            txtm.Text = "";
+            txtRez.Text = "";
+            dgvMass.Rows.Clear();
+            dgvMass.Columns.Clear();
+        }
+
+        private void cmdStart_Click(object sender, EventArgs e)
+        {
+            int n, m, nn = 0;
+            float summ = 0, sr = 0;
+            n = Int32.Parse(txtm.Text);
+            m = Int32.Parse(txtm.Text);
+            int[,] A = new int[n, m];
+            Random rnd = new Random();
+            for (int i = 0; i <= n - 1; i++)
+            {
+                for (int j = 0; j <= m - 1; j++)
+                {
+                    A[i, j] = rnd.Next(-50, 50);
+                }
+            }
+            dgvMass.RowCount = n;
+            dgvMass.ColumnCount = m;
+            for (int i = 0; i <= n - 1; i++)
+            {
+                for (int j = 0; j <= m - 1; j++)
+                {
+                    dgvMass.Rows[i].Cells[j].Value = A[i, j];
+                }
+            }
+            for (int j = 0; j <= m - 1; j++)
+                dgvMass.Columns[j].Width = 50;
+            for (int i = 0; i <= n - 1; i++)
+            {
+                for (int j = 0; j <= m - 1; j++)
+                {
+                    if (A[i, j] > 0)
+                    {
+                        summ = summ + A[i, j];
+                        nn += 1;
+                    }
+                }
+            }
+            sr = summ / nn;
+            txtRez.Text = "Среднее арифметическое =" + Math.Round(sr, 5).ToString() + Environment.NewLine + "Количество положительных элементов = " + nn.ToString();
+
+      }
+    }
+}
+
